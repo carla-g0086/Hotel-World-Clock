5dc75ad95b6e8925e30b702195fe21969cf88a41
carla-g0086 <carla.rutter1986@gmail.com>
2024-12-21 20:05:28 +0100

    formed the javascript structure with moments.js

---
+++ javascript/index.js
@@ -0,0 +1,45 @@
+function updateTime(){
+  //New York
+  let newYorkElement = document.querySelector("#NewYork");
+  let newYorkDateElement = newYorkElement.querySelector(".date");
+  let newYorkTimeElement = newYorkElement.querySelector(".time");
+  let newYorkTime = moment().tz("America/New_York");
+
+  newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
+  newYorkTimeElement.innerHTML = newYorkTime.format(
+    "h:mm:ss [<small>]A[<small>] "
+  );
+
+  //London
+  let londonElement = document.querySelector("#London");
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTimeElement = londonElement.querySelector(".time");
+  let londonTime = moment().tz("Europe/London");
+
+  londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
+  londonTimeElement.innerHTML = londonTime.format(
+    "h:mm:ss [<small>]A[<small>] "
+  );
+
+  //Paris
+  let parisElement = document.querySelector("#Paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
+
+  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[<small>] ");
+
+  //Tokyo
+  let tokyoElement = document.querySelector("#Tokyo");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+
+  tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+  tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[<small>] ");
+}
+
+
+updateTime();
+setInterval(updateTime, 1000);
